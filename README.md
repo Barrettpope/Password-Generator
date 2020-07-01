d0ebea50caa7a6ee08b23a5366a9e6ccfba57dfe
Barrett Pope <barrettpope94@gmail.com>
2020-06-30 21:08:46 -0400

    Changes made to script.js and index.html.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password-Generator
+Password-Generator is an interactive application that generates a unique password based on user-selected criteria.
+
+## Description 
+Upon entering the application, users will be greeted with a simple yet polished page featuring an empty text box with a prominent "Generate Password" button right below.
+
+Once the button is clicked, the user will be presented with a single prompt asking to choose a desired password length (between 8 and 128 characters). After a value is entered, the user will then be presented with 4 additional questions, which display in the following order:
+
+    2. Do you want your password to include lowercase letters?
+    3. Do you want your password to include uppercase letters?
+    4. Do you want your password to include numbers?
+    5. Do you want your password to include special characters?
+
+If none of the above criteria is selected, an alert will be displayed requiring the user to choose at least one character type. Then, the user will once again be looped through the questions until the requirement is met.
+
+Finally, 
+    
+
+## Support
+For questions, comments or concerns regarding the Password-Generator application, please email barrettpope94@gmail.com
+
+## Contributing
+Pull requests are welcome. 
--- script.js
+++ script.js
@@ -1,58 +1,86 @@
+// Assignment code
+var generateBtn = document.getElementById("generate");
 
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Declaring global variables
 var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChar = "0123456789";
 var specialChar = "!@#$%^&*()_+~`|}{[]:;?',./-=";
 
+// Adds event listener to generate button
+generateBtn.addEventListener("click", displayPassword);
+
+// Displays password to the text area
 function displayPassword() {
     var passwordSet = generatePassword();
     var userPassword = document.getElementById("password");
 
     userPassword.value = passwordSet;
-
 }
 
-document.getElementById("generate").addEventListener("click", displayPassword);
-
+// Generates password
 function generatePassword() {
     var generate = "";
+    var passwordSet = "";
 
-var passwordLength = Number(prompt("How long would you like your password to be? (Please select a numberical value between 8 and 128 characters)")).trim();
+    var length = prompt("How long would you like your password to be? (Please select a numerical value between 8 and 128 characters)");
 
-if(passwordLength >= 8 && passwordLength <= 128) { 
+// Assigns numerical value to length variable
+    length = parseInt(length);
 
-    var lowercase = cofirm("Do you want your password to include lowercase letters?");
-    var uppercase = confirm("Do you want your password to include uppercase letters?");
-    var number = confirm("Do you want your password to include numbers?");
-    var special = confirm("Do you want your password to include special characters?");
-
-    if(lowercase) {
-        passwordSet += lowerChar
-    }
-    if(uppercase) {
-        passwordSet += upperChar
-    }
-    if(number) {
-        passwordSet += numberChar
-    }
-    if(special) {
-        passwordSet += specialChar
+// Establishes password length + confirms user-selections for each character 
+    if (length >= 8 && length <= 128) { 
+        var lowercase = confirm("Do you want your password to include lowercase letters?");
+        var uppercase = confirm("Do you want your password to include uppercase letters?");
+        var number = confirm("Do you want your password to include numbers?");
+        var special = confirm("Do you want your password to include special characters?");
     }
 
-if (!lowercase && !uppercase && !number && !special) {
-    alert("You must choose at least one option.");
-    generatePassword();
-    userPassword = "";
-    return;
+// Creates pop-up if incorrect length is entered
+    else {
+        var resume = confirm("You must choose a numerical value between 8 and 128! \n Cancel to exit.")
+        if (resume) {
+            return generatePassword();
+        }
+        return ''
+    }
+
+// Creates pop-up if no characters are selected
+    if (!lowercase && !uppercase && !number && !special) {
+        var resume = confirm("You must choose at least one character option! \n Cancel to exit.")
+        if (resume) {
+            return generatePassword();
+        }
+        return ''
+    }
+
+// Sets selected characters to their corresponding variables
+    if (lowercase === true) {
+        generate += lowerChar
+        passwordSet += returnChar(lowerChar)
+    };
+    if (uppercase === true) {
+        generate += upperChar
+        passwordSet += returnChar(upperChar)
+    };
+    if (number === true) {
+        generate += numberChar
+        passwordSet += returnChar(numberChar)
+    };
+    if (special === true) {
+        generate += specialChar
+        passwordSet += returnChar(specialChar)
+    };
+
+// Creates for loop to generate password
+    for (var i = passwordSet.length; i < length; i++) {
+        passwordSet += returnChar(generate);
+    }
+    return passwordSet;
 }
 
-for(var i = 0; i < passwordLength; i++) {
-    userPassword += passwordSet.charAt(Math.floor(Math.random() * passwordSet.length));
-}
-
-var yourSecurePassword = document.getElementById("password");
-yourSecurePassword.innerHTML = userPassword;
-document.getElementById("password").style.color = "rgb (50, 50, 50)";
-}
+// Assigns characters at random
+function returnChar(str) {
+    return str[Math.floor(Math.random() * str.length)]
 }
 
